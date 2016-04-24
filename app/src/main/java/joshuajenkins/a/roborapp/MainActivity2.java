14f5f05c27ba09dad51247f28dab129471b26111
JJenkins <hersheyman123@hotmail.com>
2016-04-24 13:22:45 -0500

    First commit

---
+++ app/src/main/java/joshuajenkins/a/roborapp/MainActivity2.java
@@ -0,0 +1,18 @@
+package joshuajenkins.a.roborapp;
+
+/**
+ * Created by hersh on 4/22/2016.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity2 extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+}
